96791a0cb56c0dea4fcdf25280c500b76002551c
NeverL3gend <alejandrogarcicojulun@gmail.com>
2020-08-26 14:18:07 -0400

    first commit

---
+++ Python/ex2.py
@@ -0,0 +1,18 @@
+###############################################################################
+#Comments  and Pound Characters
+###############################################################################
+
+#A Comment, this is so you can read your program later.
+#Anything after the # is ignored by Python
+
+print('I could have code like this')# add the comment after is ignored
+#You can also use a comment to "disable" or comment out code
+#print('this wont run.')
+
+print('this will run.')
+
+###############################################################################
+#Study Drill
+###############################################################################
+# Q: What does the # actually do in python?
+# A: It's to create comments and as well to disable some parts of code that you do not want to run
