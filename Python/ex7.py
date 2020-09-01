a08f1c2b195c900927296c4715ae430065685767
NeverL3gend <alejandrogarcicojulun@gmail.com>
2020-09-01 13:44:53 -0400

    commiting

---
+++ Python/ex7.py
@@ -0,0 +1,45 @@
+################################################################################
+# More Printing
+################################################################################
+# going to print out the string
+print('mary had a little lamb')
+#going to print out the string and instert snow in the empty bracket(different f string)
+weather = 'snow'
+print(f'its fleece was white as {weather}')
+#going to print out the string
+print('and anywhere that mary went.')
+#going to print out 10 dots
+print('.' * 10)# what does it do?  it's going to print the decimal 10 times
+
+#going to print c
+end1 = 'c'
+#going to print h
+end2 = 'h'
+#going to print e
+end3 = 'e'
+#going to print e
+end4 = 'e'
+#going to print s
+end5 = 's'
+#going to print e
+end6 = 'e'
+#going to print b
+end7 = 'b'
+#going to print u
+end8 = 'u'
+#going to print r
+end9 = 'r'
+#going to print g
+end10 = 'g'
+#going to print e
+end11 = 'e'
+#going to print r
+end12 = 'r'
+meal = 'with fries and shake'
+#watch end = '' at the end. try removing it to see what happens
+#Going to print Cheeseburger
+#if you remove the end = ' ' you're going to get cheese burger
+#seems that the end = ' ' concats any word with the word end being apart of it
+
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ' )
+print(end7 + end8 + end9 + end10 + end11 + end12, meal)
