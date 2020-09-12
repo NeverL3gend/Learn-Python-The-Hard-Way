0054d91b267b161555407bc70ca8d109917d9aa6
Neverl3gend <alejandrogarcicojulun@gmail.com>
2020-09-12 11:05:19 -0400

    new commit

---
+++ Python/ex11.py
@@ -0,0 +1,70 @@
+################################################################################
+# Ask Questions
+################################################################################
+
+# print('how old are you?', end = ' ') #answer will be right next to the question
+# age = input()
+#
+# print('how tall are you?', end = ' ') #answer will be right next to the question
+# height = input()
+#
+# print('how much do you weigh?', end = ' ') #answer will be right next to the question
+# weight = input()
+#
+# #end = ' ' helps tell print to not end the line with a new line character
+# #and go to the next line
+#
+# print(f"So, you're {age} old, {height} tall and {weight} heavy.")v
+#
+#
+# #this is without end = ' '
+# print('who are you?') #answer will be on a new line
+# who = input()
+#
+# print('how old are you?') #answer will be a on a new line
+# age = input()
+#
+# print('what did you eat?') #answer will be on a new line
+# food = input()
+#
+# print(f"this is where {who} goes and this is where {age} goes and finally what you ate goes here {food}")
+
+
+# print('what is your name')
+# x = input("enter name here") #shows the string before the input is submitted
+# y = input("enter age here")
+# print("your name is", x , "and your age is", y)
+
+#testing with integers
+# x = input('enter an integer')
+# print("you have entered", x)
+
+# x = input('enter an integer >>>') #the integer is going to be a string
+# y = int(x) #once the integer goes through x, y will convert it into an int
+# print("you have entered", y)
+
+
+################################################################################
+#differences between input and raw_input
+################################################################################
+
+'''
+input function is used to take the values from the user. The functions is called
+to tell the program to stop and wait for the user to input the avlues.
+
+also input function converts the input you gave into a string
+'''
+
+'''
+raw_input function is used to get the values from the user as well. When this
+function is called it will tell the program to stop and wait for the user input The
+values
+
+'''
+
+################################################################################
+#using float while using input()
+
+# x = float(input())
+# print(x)
+# print(type(x))
