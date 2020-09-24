14ead2361d85ad2717364dfafa6bfe2696a42483
Neverl3gend <alejandrogarcicojulun@gmail.com>
2020-09-23 21:12:13 -0400

    commit

--- Python/ex15.py
+++ Python/ex15.py
@@ -20,14 +20,28 @@ from sys import argv
 # txt_again = open(file_again)
 # #this is going to show what is in the file itself
 # print(txt_again.read())
+################################################################################
+#second attempt to understand reading files
+################################################################################
+#from sys import argv
+#we are accessing our library sys and from there importing our script argv
 script, filename = argv
-
+#script is the name of the file we are using when running the script
+#filename is the name of the file that we are trying to access during this excercise
 txt = open(filename)
-
-print(f"Here's your {filename}")
+#we have a variable called txt which is going to hold the value which allows us
+#to open our file once we give our filename a value
+print(f"Here's your file {filename}")
+#we are using our f-string here to see the name of the file that we are using
 print(txt.read())
-
-name_of_file = input("enter the correct file name here > ")
-opener = open(name_of_file)
-print(opener.read())
-print(opener.close())
+#here we are seeing that we are reading the document that is written inside the txt file
+print("type the filename again:")
+#we are asking the user to type in the file name again
+file_again = input("> ")
+# here is where the user enters in the file name
+text_again = open(file_again)
+# After file_again entry has been submitted text_again will run and will open up
+# our filename that we have entered and read the file
+print(text_again.read())
+# after text_again has been completed we are going to run the read function and
+# read the value of the file aka text
--- README.md
+++ README.md
@@ -5,5 +5,5 @@ for some time now I've been wanting to learn Python. I've been interested in bui
 
 ## What do you want to do ?
 I want to main in Python only and I want to be able to build robotic arms and cool scripts that allows you to do really fun things.
-## September 21 2020, What did you learn today ?
-Currently learning how to open/close/read/delete files while using python. Currently on EX16 but I need to go back to EX15 to get a better understanding of opening and reading files. I understand it but not as much as I would like. I've also decided that on weekends I will not code but will code Mon - fri and upload to GH. That is my current status.
+## September 23 2020, What did you learn today ?
+Reviewing EX15 and EX16. Went over EX15 today and have a better understanding. Going over EX16 tomorrow
